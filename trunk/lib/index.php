35e45be24e72fc3c2dd867b0b39bcb194a29009c
jazzyjames <jazzyjames@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-25 18:34:25 +0000

    First launch control submission 25/05/13 @ 19:33
    git-svn-id: https://plugins.svn.wordpress.org/launch-control@718283 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/lib/index.php
@@ -0,0 +1,204 @@
+<?php
+		  global $wpdb, $error, $wp_query, $current_theme_locale_name, $mt_lib;
+		
+		  if (!is_array($wp_query->query_vars))
+						   $wp_query->query_vars = array();
+ 		  
+		  $action = $_REQUEST['action'];
+		  $info  = $error =  '';
+		  nocache_headers();
+		  $class_login = "user-icon";
+		  $class_password  = "pass-icon";
+		  
+		  header('Content-Type: '.get_bloginfo('html_type').'; charset='.get_bloginfo('charset'));
+		  
+		  if ( defined('RELOCATE') ) 
+		  { // Move flag is set
+			if ( isset( 	$_SERVER['PATH_INFO'] ) && ($_SERVER['PATH_INFO'] != $_SERVER['PHP_SELF']) )
+							$_SERVER['PHP_SELF'] = str_replace( $_SERVER['PATH_INFO'], '', $_SERVER['PHP_SELF'] );
+		  
+							 $schema = ( isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ) ? 'https://' : 'http://';
+			if ( dirname($schema . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']) != get_settings('siteurl') )
+				  update_option('siteurl', dirname($schema . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']) );
+		  }
+		
+		  switch($_REQUEST["action"])
+		  {
+		  case 'login' : 
+			default:
+				$user_login = '';
+				$user_pass = '';
+				$using_cookie = false;
+				
+				 if ( !isset( $_REQUEST['redirect_to'] ) ) {
+					$redirect_to = get_settings('siteurl');
+				 } else {
+					$redirect_to = $_REQUEST['redirect_to'];
+				 }
+				 
+				 if(isset($_SESSION['redirect_me_back'])) $redirect_to = $_SESSION['redirect_me_back'];
+		
+				if( $_POST ) {
+					$user_login = $_POST['log'];
+					$user_login = sanitize_user( $user_login );
+					$user_pass  = $_POST['pwd'];
+					$rememberme = $_POST['rememberme'];
+					
+					$access = array();
+						$access['user_login'] 		  = $user_login;
+						$access['user_password']  = $user_pass;
+						$access['remember'] = false;
+						$user = wp_signon( $access, false);
+						
+						if ( is_wp_error($user) )  {
+							$info  =  'You entered your login and password are incorrect!';
+							$class_login = 'error-login';
+							$class_password = 'error-password';
+						} 
+						
+				} else {
+					if (function_exists('wp_get_cookie_login'))		
+					{
+						$cookie_login = wp_get_cookie_login();
+						if ( ! empty($cookie_login) ) {
+							$using_cookie = true;
+							$user_login = $cookie_login['login'];
+							$user_pass = $cookie_login['password'];
+						}
+					}
+					elseif ( !empty($_COOKIE) ) 
+					{
+						if ( !empty($_COOKIE[USER_COOKIE]) )
+							$user_login = $_COOKIE[USER_COOKIE];
+						if ( !empty($_COOKIE[PASS_COOKIE]) ) {
+							$user_pass = $_COOKIE[PASS_COOKIE];
+							$using_cookie = true;
+						}
+					}
+				}
+			
+				do_action('wp_authenticate', array(&$user_login, &$user_pass));
+				if ( $user_login && $user_pass ) {
+					$user = new WP_User(0, $user_login);
+
+					if ( wp_login($user_login, 
+										   $user_pass, 
+										   $using_cookie) ) {
+						if ( !$using_cookie )
+							wp_setcookie($user_login, $user_pass, false, '', '', $rememberme);
+							do_action('wp_login', $user_login);
+							wp_redirect($redirect_to);
+						exit;
+					} else {
+						if ( $using_cookie )			
+							  $error = __('Your session has expired.',$current_theme_locale_name);
+					}
+				} 
+		?>
+		
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo $mt_lib['page_title']; ?></title>
+
+<link rel="stylesheet" type="text/css" href="<?php echo PLUGIN_URL . LIB_DIR; ?>/style.css" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
+<?php
+	if ( $mt_lib['expiry_date'] ):
+		list( $date,$time ) = explode( '|', $mt_lib['expiry_date'] );
+		list( $month, $day, $year ) = explode( '.', $date );
+		list( $hours, $minutes, $seconds ) = explode ( ':', $time );
+		$month--;
+?>
+
+<script type="text/javascript" src="<?php echo PLUGIN_URL . LIB_DIR; ?>/js/jquery.countdown.js"></script>
+<script type="text/javascript">
+$(function () {
+var austDay = new Date(<?php echo "$year, $month, $day, $hours, $minutes, $seconds, 0"; ?>);
+	$('#defaultCountdown').countdown({until: austDay, layout: '{dn} {dl}, {hn} {hl}, {mn} {ml}, and {sn} {sl}'});
+	$('#year').text(austDay.getFullYear());
+});
+</script>
+<?php endif; ?>
+
+<?php if ( $mt_lib['body_bg'] || $mt_lib['body_bg_color'] ): ?>
+			<style type="text/css"> 
+				body { background: 
+					<?php echo $mt_lib['body_bg_color']; ?> 
+						<?php if ( $mt_lib['body_bg'] )  {
+								echo "url({$mt_lib['body_bg']}) repeat-x scroll 0 0"; 
+								} else {
+								echo 'url('. PLUGIN_URL . LIB_DIR . '/images/default-bg.png' .') repeat-x scroll 0 0';
+								} 
+								
+								?>;}
+								
+			</style>
+<?php endif; ?>  
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$(".username").focus(function() 	{ $(".user-icon").css("left","-49px");});
+	$(".username").blur(function()   	{ $(".user-icon").css("left","0px"); });
+	$(".password").focus(function() 	{ $(".pass-icon").css("left","-49px");});
+	$(".password").blur(function()   	{ $(".pass-icon").css("left","0px");});
+});
+		   
+</script>
+<script type="text/javascript" src="<?php echo PLUGIN_URL . LIB_DIR ?>/js/jquery.placeholder.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    $('input[placeholder]').placeholder();
+});
+
+$(window).load(function() {
+	var vWrapWidth  = -$("#wrapper").width()/2;
+	var vWrapHeight = -$("#wrapper").height()/2;
+	       $("#wrapper").css({'margin-left' : vWrapWidth});
+		   $("#wrapper").css({'margin-top' : vWrapHeight});
+		   
+	var vCompWidth    = -$(".company a img").width()/2;
+	       $(".company a img").css({'margin-left' : vCompWidth});
+});
+	
+</script>
+</head>
+<body>	
+        <div id="wrapper">
+				<div class="company">
+					<a href="<?php bloginfo('home'); ?>"><img src="<?php if ( $mt_lib['logo'] ) { echo $mt_lib['logo']; } else { echo PLUGIN_URL . LIB_DIR.'/images/launchcontrol-logo.png'; } ?>" alt="logo"/></a>
+				</div>
+
+
+							<center> <div style="display:"> <!-- Registration -->  
+            <div id="register-form">  
+            <div class="title">  
+                <h1>Register your Account</h1>  
+                <span>Sign Up with us and Enjoy!</span>  
+            </div>  
+                <form action="<?php echo site_url('wp-login.php?action=register', 'login_post') ?>" method="post">  
+                <input type="text" name="user_login" placeholder="Username" id="user_login" class="input" />  
+                <input type="text" name="user_email" placeholder="Your E-mail" id="user_email" class="input"  /> 
+                  <?php do_action('register_form'); ?>  
+                    <input type="submit" value="Register" id="register" />
+						<input type="hidden" name="redirect_to" value=""/>
+						<br>
+                </hr>  
+                <strong><p class="statement">A password will be e-mailed to you.</p></strong>
+                  <br>
+
+
+				
+                </form>  
+            </div>  
+    </div></center><!-- /Registration --> 
+				
+	</div> <!-- end wrapper -->		
+</body>
+</html>
+<?php	die(); 
+				break;  
+				} ?>
---
+++ trunk/page-options.php
@@ -0,0 +1,77 @@
+<?php
+
+	function mt_manage_options()
+	{
+		global $mt_option;
+		
+		$mt_option = mt_get_option();
+		
+		if ( $_POST ) {
+				  $state = $_POST['state']; 
+  			  if (!$state)   { $mt_option['state'] = 'live';  } 
+						else { $mt_option['state'] = 'launchcontrol'; }
+			
+			$mt_option['lib_options'] = $_POST['lib_options'];
+			
+			/* trigger theme activation */
+			if ( file_exists( dirname( __FILE__ ).'/'.LIB_DIR.'/functions.php' ) ) {
+			 include_once dirname( __FILE__ ).'/'.LIB_DIR.'/functions.php';
+   		     $mt_option = apply_filters( 'lib_update', $mt_option );
+			}
+			
+			/* counter */
+			$mt_option['expiry_date'] = $_POST['lib_options']['expiry_date'] ? $_POST['lib_options']['expiry_date'] : '';
+			if ( !$mt_option['expiry_date'] ) {
+	   unset( $mt_option['expiry_date'] );
+														}
+			mt_update_option($mt_option);
+		}
+		
+		mt_get_lib_var();
+		?>
+		
+		<div id="launchcontrol-options" class="wrap">	
+			<form method="post" action="" enctype="multipart/form-data" name="options-form">
+					<div id="general">
+						<div class="title">
+							<img src="<?php echo PLUGIN_URL ?>images/icon.png" alt="Logo" class="logo" />
+							<h1>Launch Control -</h1>						
+							<input name="state" type="checkbox" id="ch_location" name="ch_location"  <?php if ( $mt_option['state'] == 'launchcontrol' )  echo 'checked="true"' ?> />
+						</div>						
+				
+						<div class="theme-options">
+							<?php  include_once dirname( __FILE__ ).'/'.LIB_DIR.'/options.php';  ?>
+							 <input type="submit" id="mt-submit" name="save_changes" class="button-primary" value="<?php _e('Save changes', 'launchcontrol');?>" />
+						</div>  
+					</div>					 
+			</form>
+			
+			<!-- Contact Support-->
+			<div id="contact-support">
+			<strong>Help us by making a donation of your choice, to develop this plugin even further. <a href="http://swapycle.co.uk/why-us/" title="Custom Login Redirect" target="">Why You Should Donate</a></strong><p>
+			
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="MXLYGA4B92K36">
+<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+			
+			
+				<a href="mailto:info@swapycle.co.uk" title="<?php _e('Contact Support', 'launchcontrol');?>" target="_blank">
+					<?php _e('Contact Support', 'launchcontrol');?>
+				</a><br><p> 
+				<a href="http://wordpress.org/plugins/custom-login-redirect/" title="Custom Login Redirect" target="_blank">Redirect users to custom page on login</a>
+				<p>
+				<a href="http://wordpress.org/plugins/wp-approve-user/" title="WP Approve User" target="_blank">Approve or unapprove user registrations</a>
+							</div>
+			<!-- End Contact Support-->		
+			
+		</div>
+		
+		<?php
+	
+	}
+
+?>
